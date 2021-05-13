0703d630e0028c60a0cc9b7d90619b16c1e326d2
Alex-Hub-cyber <gl0430032020@unab.edu.sv>
2021-05-13 12:22:29 -0500

    Progranación.I
    Tablas con Crud.

---
+++ SistemaVentasP2/SistemaVentasP2/VISTA/FrmLogin.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace SistemaVentasP2.VISTA
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.lblIngrese = new System.Windows.Forms.Label();
+            this.LblRegistrar = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(159, 129);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(201, 20);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(159, 204);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(201, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Email :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(43, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password :";
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(189, 263);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(118, 35);
+            this.btnEntrar.TabIndex = 4;
+            this.btnEntrar.Text = "Acceder";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // lblIngrese
+            // 
+            this.lblIngrese.AutoSize = true;
+            this.lblIngrese.Font = new System.Drawing.Font("Modern No. 20", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngrese.Location = new System.Drawing.Point(74, 41);
+            this.lblIngrese.Name = "lblIngrese";
+            this.lblIngrese.Size = new System.Drawing.Size(306, 25);
+            this.lblIngrese.TabIndex = 5;
+            this.lblIngrese.Text = "Ingresar al Menu principal";
+            // 
+            // LblRegistrar
+            // 
+            this.LblRegistrar.AutoSize = true;
+            this.LblRegistrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblRegistrar.Location = new System.Drawing.Point(201, 328);
+            this.LblRegistrar.Name = "LblRegistrar";
+            this.LblRegistrar.Size = new System.Drawing.Size(89, 16);
+            this.LblRegistrar.TabIndex = 6;
+            this.LblRegistrar.TabStop = true;
+            this.LblRegistrar.Text = "Registrarse";
+            this.LblRegistrar.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LblRegistrar_LinkClicked);
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(451, 473);
+            this.Controls.Add(this.LblRegistrar);
+            this.Controls.Add(this.lblIngrese);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmUsuarios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Label lblIngrese;
+        private System.Windows.Forms.LinkLabel LblRegistrar;
+    }
+}
---
+++ SistemaVentasP2/SistemaVentasP2/VISTA/FrmVentas.cs
@@ -0,0 +1,48 @@
+﻿using SistemaVentasP2.MODEL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaVentasP2.VISTA
+{
+    public partial class FrmVentas : Form
+    {
+        public FrmVentas()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmVentas_Load(object sender, EventArgs e)
+        {
+            using (sistema_ventasEntities db = new sistema_ventasEntities())
+            {
+                try
+                {
+                    var consultacliente = db.tb_cliente.ToList();
+
+                    comboBox2.DataSource = consultacliente;
+                    comboBox2.DisplayMember = "nombreCliente";
+                    comboBox2.ValueMember = "iDCliente";
+
+                    var consultadocumento = db.tb_documento.ToList();
+
+                    comboBox1.DataSource = consultadocumento;
+                    comboBox1.DisplayMember = "nombreDocumento";
+                    comboBox1.ValueMember = "iDDocumento";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+       
+    }
+}
